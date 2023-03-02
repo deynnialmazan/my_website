be72339c7687c7c42af160eb72017ed599b76762
Deynni Almazan <deynnialmazan@hotmail.com>
2023-03-02 11:43:32 -0600

    Change responsive design

--- assets/style/index.js
+++ assets/style/index.js
@@ -3,15 +3,18 @@
 const hamburguerIcon = document.querySelector('.h-icon');
 const menu = document.querySelector('.menu-header');
 const menuItems = document.querySelectorAll('.menu-items');
+const nameHeader = document.querySelector('.name-header')
 
 let statusIcon= 'hide';
 hamburguerIcon.addEventListener('click', () => {
     if (statusIcon ==='hide') {
         menu.style.display= 'block';
         statusIcon = 'show';
+        nameHeader.innerText= '';
     } else if (statusIcon === 'show') {
         menu.style.display= 'none';
         statusIcon = 'hide';
+        nameHeader.innerText= 'deynni almazan';
     }
 });
 
